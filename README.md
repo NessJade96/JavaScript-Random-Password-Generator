fffa836bce09746806b24669b328e50905ee7b27
Vanessa Bloom <ness.jade@outlook.com>
2022-05-31 09:13:21 +1000

    functions to create a prompt and confirm prompts for each criteria.
    Using the provided script.js code, I am creating a generatePassword function to start the prompts - within this function it will call the getPasswordLength function to return the value of the password length. Then I have created confirm prompts for the Special characters to return a true or false value and so on for each of the passwords criteria.

--- README.md
+++ README.md
@@ -23,4 +23,108 @@ The password is then created and displayed in an alert or written to the page.
 # Commit notes:
 
 Commit 1:
-Created repo on Gibhub, then copied the supplied HTML, CSS, and Javascript files. Created the README.md.
+Created repo on Gibhub, then copied the supplied HTML, CSS, and Javascript files. Created the README.md, and made planning notes.
+
+Commit 2:
+Using the provided script.js code, I am creating a generatePassword function to start the prompts - within this function it will call the getPasswordLength function to return the value of the password length. Then I have created confirm prompts for the Special characters to return a true or false value and so on for each of the passwords criteria.
+
+//Array of special chartacters
+var specialCharacters = [
+'@',
+'%',
+'+',
+'\\',
+'/',
+"'",
+'!',
+'#',
+'$',
+'^',
+'?',
+':',
+',',
+')',
+'(',
+'}',
+'{',
+']',
+'[',
+'~',
+'-',
+'_',
+'.'
+];
+​
+// Array of numeric characters
+var numericCharacters = [
+'0',
+'1',
+'2',
+'3',
+'4',
+'5',
+'6',
+'7',
+'8',
+'9'
+];
+​
+// Array of lowercase characters
+var lowerCasedCharacters = [
+'a',
+'b',
+'c',
+'d',
+'e',
+'f',
+'g',
+'h',
+'i',
+'j',
+'k',
+'l',
+'m',
+'n',
+'o',
+'p',
+'q',
+'r',
+'s',
+'t',
+'u',
+'v',
+'w',
+'x',
+'y',
+'z'
+];
+​
+// Array of uppercase characters
+var upperCasedCharacters = [
+'A',
+'B',
+'C',
+'D',
+'E',
+'F',
+'G',
+'H',
+'I',
+'J',
+'K',
+'L',
+'M',
+'N',
+'O',
+'P',
+'Q',
+'R',
+'S',
+'T',
+'U',
+'V',
+'W',
+'X',
+'Y',
+'Z'
+];
--- script.js
+++ script.js
@@ -1,116 +1,83 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getSpecialCharacters() {
+	return confirm(
+		"Would you like your generated password to contain special characters?"
+	);
+}
+
+function getNumericCharacters() {
+	return confirm(
+		"Would you like your generated password to contain numeric characters?"
+	);
+}
+
+function getLowerCaseCharacters() {
+	return confirm(
+		"Would you like your generated password to contain LowerCase characters?"
+	);
+}
+
+function getUpperCaseCharacters() {
+	return confirm(
+		"Would you like your generated password to contain UpperCase characters?"
+	);
+}
+
+//this function will return the clients input value of character length:
+function getPasswordLength() {
+	var passwordLength = prompt(
+		"Please choose password length (min 8 characters, max 128 characters)"
+	);
+	if (passwordLength >= 8 && passwordLength <= 128) {
+		return passwordLength;
+	} else {
+		return getPasswordLength();
+	}
+}
+
+// For the client to input character length:
+function generatePassword() {
+	// Object that holds the chosen values:
+	var passwordValues = {
+		length: null,
+		specialCharacters: null,
+		numericCharacters: null,
+		lowerCaseCharacters: null,
+		upperCaseCharacters: null,
+	};
+
+	passwordValues.length = getPasswordLength();
+	passwordValues.specialCharacters = getSpecialCharacters();
+	passwordValues.numericCharacters = getNumericCharacters();
+	passwordValues.lowerCaseCharacters = getLowerCaseCharacters();
+	passwordValues.upperCaseCharacters = getUpperCaseCharacters();
+	console.log(
+		"🚀 ~ file: script.js ~ line 44 ~ generatePassword ~ passwordValues",
+		passwordValues
+	);
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+	passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-//Array of special chartacters
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-​
-// Array of numeric characters 
-var numericCharacters = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9'
- ];
-​
-// Array of lowercase characters 
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-​
-// Array of uppercase characters 
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+// function startGame() {
+// 	var playerChoice = prompt("rock, paper or scissors?");
+// 	if (validOptions.includes(playerChoice)) {
+// 		var computerChoice =
+// 			validOptions[Math.floor(Math.random() * validOptions.length - 1)];
+// 		var result = getResult(playerChoice, computerChoice);
+// 		playAgain(result);
+// 	} else {
+// 		startGame();
+// 	}
+// }
