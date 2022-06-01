e07e7191f92e4f8f57e2172f973bed212acd19f2
Vanessa Bloom <ness.jade@outlook.com>
2022-06-01 15:15:23 +1000

    For Loop to test password Values and output the password
    Creating a for loop that takes in the values of the true/false from the passwordValues prompts - that tests if it is upper, lower, number, or special. If it is any of those values then it will input one element from the array at random. This will return a randomly generated password with the selected criteria. Added in screenshots and links :)

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # JavaScript-Random-Password-Generator
 
+# Description:
+
 A simple random password generator made using HTML, CSS, and JavaScript.
 
 # Planning Notes:
@@ -14,11 +16,15 @@ At least one of the character type prompts has to be selected, as well as the pa
 
 The password is then created and displayed in an alert or written to the page.
 
-# Description:
-
 # Screenshots:
 
+![Screenshot1](./Screenshots/Screenshot%202022-06-01%20151041.jpg)
+![Screenshot2](./Screenshots/Screenshot%202022-06-01%20151119.jpg)
+
 # Links:
+
+GitHub repo: https://github.com/NessJade96/JavaScript-Random-Password-Generator
+Deploy: https://nessjade96.github.io/JavaScript-Random-Password-Generator/
 
 # Commit notes:
 
@@ -28,103 +34,5 @@ Created repo on Gibhub, then copied the supplied HTML, CSS, and Javascript files
 Commit 2:
 Using the provided script.js code, I am creating a generatePassword function to start the prompts - within this function it will call the getPasswordLength function to return the value of the password length. Then I have created confirm prompts for the Special characters to return a true or false value and so on for each of the passwords criteria.
 
-//Array of special chartacters
-var specialCharacters = [
-'@',
-'%',
-'+',
-'\\',
-'/',
-"'",
-'!',
-'#',
-'$',
-'^',
-'?',
-':',
-',',
-')',
-'(',
-'}',
-'{',
-']',
-'[',
-'~',
-'-',
-'_',
-'.'
-];
-​
-// Array of numeric characters
-var numericCharacters = [
-'0',
-'1',
-'2',
-'3',
-'4',
-'5',
-'6',
-'7',
-'8',
-'9'
-];
-​
-// Array of lowercase characters
-var lowerCasedCharacters = [
-'a',
-'b',
-'c',
-'d',
-'e',
-'f',
-'g',
-'h',
-'i',
-'j',
-'k',
-'l',
-'m',
-'n',
-'o',
-'p',
-'q',
-'r',
-'s',
-'t',
-'u',
-'v',
-'w',
-'x',
-'y',
-'z'
-];
-​
-// Array of uppercase characters
-var upperCasedCharacters = [
-'A',
-'B',
-'C',
-'D',
-'E',
-'F',
-'G',
-'H',
-'I',
-'J',
-'K',
-'L',
-'M',
-'N',
-'O',
-'P',
-'Q',
-'R',
-'S',
-'T',
-'U',
-'V',
-'W',
-'X',
-'Y',
-'Z'
-];
+Commit 3:
+Creating a for loop that takes in the values of the true/false from the passwordValues prompts - that tests if it is upper, lower, number, or special. If it is any of those values then it will input one element from the array at random. This will return a randomly generated password with the selected criteria. Added in screenshots and links :)
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//This function will return true or false into the passwordValues in the function generatePassword()
 function getSpecialCharacters() {
 	return confirm(
 		"Would you like your generated password to contain special characters?"
 	);
 }
+
+//This function will return true or false into the passwordValues in the function generatePassword()
 
 function getNumericCharacters() {
 	return confirm(
@@ -13,11 +16,15 @@ function getNumericCharacters() {
 	);
 }
 
+//This function will return true or false into the passwordValues in the function generatePassword()
+
 function getLowerCaseCharacters() {
 	return confirm(
 		"Would you like your generated password to contain LowerCase characters?"
 	);
 }
+
+//This function will return true or false into the passwordValues in the function generatePassword()
 
 function getUpperCaseCharacters() {
 	return confirm(
@@ -37,6 +44,14 @@ function getPasswordLength() {
 	}
 }
 
+function getCharacterFromArray(characterArray) {
+	var randomCharacter =
+		characterArray[Math.floor(Math.random() * characterArray.length - 1)];
+	randomPasswordGenerated.push(randomCharacter);
+}
+
+var randomPasswordGenerated = [];
+
 // For the client to input character length:
 function generatePassword() {
 	// Object that holds the chosen values:
@@ -48,6 +63,7 @@ function generatePassword() {
 		upperCaseCharacters: null,
 	};
 
+	// these functions create prompt boxes and return boolean into the above passwordValues object:
 	passwordValues.length = getPasswordLength();
 	passwordValues.specialCharacters = getSpecialCharacters();
 	passwordValues.numericCharacters = getNumericCharacters();
@@ -57,6 +73,36 @@ function generatePassword() {
 		"🚀 ~ file: script.js ~ line 44 ~ generatePassword ~ passwordValues",
 		passwordValues
 	);
+
+	var characterType = ["upper", "lower", "number", "special"];
+
+	// this creates a for loop that will generate a random password
+
+	for (var i = 0; i < passwordValues.length; i++) {
+		characterTypeRandom =
+			characterType[Math.floor(Math.random() * characterType.length)];
+		console.log(
+			"🚀 ~ file: script.js ~ line 76 ~ generatePassword ~ characterTypeRandom",
+			characterTypeRandom
+		);
+
+		if (characterTypeRandom === "upper") {
+			getCharacterFromArray(upperCasedCharactersArray);
+		}
+
+		if (characterTypeRandom === "lower") {
+			getCharacterFromArray(lowerCasedCharactersArray);
+		}
+
+		if (characterTypeRandom === "number") {
+			getCharacterFromArray(numericCharactersArray);
+		}
+
+		if (characterTypeRandom === "special") {
+			getCharacterFromArray(specialCharactersArray);
+		}
+	}
+	return randomPasswordGenerated.join("");
 }
 
 // Write password to the #password input
@@ -69,6 +115,96 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Array of special chartacters
+var specialCharactersArray = [
+	"@",
+	"%",
+	"+",
+	"\\",
+	"/",
+	"'",
+	"!",
+	"#",
+	"$",
+	"^",
+	"?",
+	":",
+	",",
+	")",
+	"(",
+	"}",
+	"{",
+	"]",
+	"[",
+	"~",
+	"-",
+	"_",
+	".",
+];
+
+// Array of numeric characters
+var numericCharactersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Array of lowercase characters
+var lowerCasedCharactersArray = [
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+];
+
+// Array of uppercase characters
+var upperCasedCharactersArray = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+];
 
 // function startGame() {
 // 	var playerChoice = prompt("rock, paper or scissors?");
